80ae249ff730b175a78349ef958d656ccf49b535
Brandon Whaley <redkrieg@gmail.com>
2016-04-09 01:31:47 -0400

    first swing at smart serialization for world data

---
+++ test_encoding.py
@@ -0,0 +1,49 @@
+import json
+
+class Thing:
+    def __init__(self, name, *things):
+        self.name = name
+        self.things = things
+
+    def repr_json(self):
+        return {"name": self.name, "things": self.things}
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(data["name"], *data["things"])
+
+class Encoder(json.JSONEncoder):
+    def default(self, o):
+        if hasattr(o, 'repr_json'):
+            if self.encoded.get(id(o), False):
+                return {"__backref__": id(o)}
+            self.encoded[id(o)] = True
+            return {
+                '__class__': o.__class__.__name__,
+                '__id__': id(o),
+                'data': o.repr_json()
+            }
+        return super().default(o)
+    def __init__(self, *args, **kwargs):
+        self.encoded = {}
+        return super().__init__(*args, **kwargs)
+
+class Decoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        self.decoded = {}
+        self.known_classes = {
+            "Thing": Thing
+        }
+        return super().__init__(*args, object_hook=self.dict_to_obj, **kwargs)
+
+    def dict_to_obj(self, d):
+        if '__backref__' in d:
+            return self.decoded[d['__backref__']]
+        if '__class__' not in d:
+            return d
+        _class = self.known_classes[d['__class__']]
+        _id = d['__id__']
+        data = d['data']
+        result = _class.from_json(data)
+        self.decoded[_id] = result
+        return result
